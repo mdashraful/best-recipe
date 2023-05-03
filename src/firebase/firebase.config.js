adaa354b7cc5dcfddaeab5952d63821673f995d5
mdashraful <mdashrafulislam369@gmail.com>
2023-05-04 02:43:31 +0600

    firebase installed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDr-Gsqwiwmsgmjcza3aS--DcbyYEcY1b8",
+  authDomain: "best-recipe-86815.firebaseapp.com",
+  projectId: "best-recipe-86815",
+  storageBucket: "best-recipe-86815.appspot.com",
+  messagingSenderId: "588559013585",
+  appId: "1:588559013585:web:4d931ec6e6bf9f815b57f0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
